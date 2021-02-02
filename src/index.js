24890b9cfa0771476f1bdb45d7bb00d6d6e42777
Podexus23 <santon23@gmail.com>
2021-02-02 16:47:48 +0300

    feat: made decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+ let basic = ""
+ const space = "**********"
+
+ const recoder = (str) =>{
+    let word = ''
+    for(let i = 0; i<str.length ; i +=2){
+       let piece = '';
+       (i !== 8)? piece = str.slice(i, (str.length + (-8+i))): piece = str.slice(i);
+       if(piece === "00") word += '';
+       if(piece === "10") word += '.';
+       if(piece === "11") word += '-';
+    }
+    return MORSE_TABLE[word]
+ }
+
+ for(let i = 0; i<expr.length; i += 10){
+   let decripta = '';
+   (i !== expr.length-10)? decripta = expr.slice(i , -(expr.length -10 - i)): decripta = expr.slice(i);
+   (decripta === space)? basic += ' ': basic += recoder(decripta);
+ }
+ return basic
 }
 
 module.exports = {
